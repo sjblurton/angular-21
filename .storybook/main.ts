b1611bf93ebfbfc36c0ae74b80cc7bb68f761b10
Simon Blurton <sblurton@hotmail.co.uk>
2026-03-18 13:38:36 +0000

    refactor(storybook): simplify configuration syntax and improve formatting

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,25 +1,18 @@
 import type { StorybookConfig } from '@storybook/angular';
 
 const config: StorybookConfig = {
-  "stories": [
-    "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
-  ],
-  "staticDirs": [
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  staticDirs: [
     {
-      "from": "../coverage/angular-21",
-      "to": "/reports/coverage"
+      from: '../coverage/angular-21',
+      to: '/reports/coverage',
     },
     {
-      "from": "../playwright-report",
-      "to": "/reports/playwright"
-    }
+      from: '../playwright-report',
+      to: '/reports/playwright',
+    },
   ],
-  "addons": [
-    "@storybook/addon-a11y",
-    "@storybook/addon-docs",
-    "@storybook/addon-onboarding"
-  ],
-  "framework": "@storybook/angular"
+  addons: ['@storybook/addon-a11y', '@storybook/addon-docs', '@storybook/addon-onboarding'],
+  framework: '@storybook/angular',
 };
 export default config;
