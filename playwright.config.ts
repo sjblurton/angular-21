375d74fa2eb6d8efaabb07c0368cfd0275040b5c
Simon Blurton <sblurton@hotmail.co.uk>
2026-03-19 12:16:59 +0000

    fix(playwright): refactor CI-related configurations for clarity and consistency

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,33 +3,35 @@
 import { defineConfig, devices } from '@playwright/test';
 
 const isVRTMode = !!process.env['VRT'];
+const isCI = !!process.env['CI'];
 
 export default defineConfig({
   testDir: './e2e',
   testMatch: isVRTMode ? '**/vrt/**/*.spec.ts' : '**/*.spec.ts',
   testIgnore: isVRTMode ? undefined : '**/vrt/**',
   fullyParallel: true,
-  forbidOnly: !!process.env['CI'],
-  retries: process.env['CI'] ? 2 : 0,
-  workers: process.env['CI'] ? 1 : undefined,
+  forbidOnly: isCI,
+  retries: isVRTMode ? 0 : isCI ? 2 : 0,
+  workers: isVRTMode ? (isCI ? 2 : undefined) : isCI ? 1 : undefined,
   reporter: 'html',
   snapshotPathTemplate: 'snapshots/{testFilePath}/{arg}{ext}',
   use: {
     baseURL: isVRTMode ? 'http://localhost:6006' : 'http://localhost:4200',
-    trace: 'on-first-retry',
+    trace: isVRTMode ? 'off' : 'on-first-retry',
   },
   webServer: isVRTMode
     ? {
-        command:
-          'test -d storybook-static || npx storybook build; npx serve storybook-static -l 6006',
+        command: isCI
+          ? 'npx serve storybook-static -l 6006'
+          : 'test -d storybook-static || npx storybook build; npx serve storybook-static -l 6006',
         url: 'http://localhost:6006',
-        reuseExistingServer: !process.env['CI'],
+        reuseExistingServer: !isCI,
         timeout: 120_000,
       }
     : {
         command: 'npm run start',
         url: 'http://localhost:4200',
-        reuseExistingServer: !process.env['CI'],
+        reuseExistingServer: !isCI,
         timeout: 120_000,
       },
   projects: [
