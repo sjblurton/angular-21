ecd1bc044d9d90d36f9655b19edea399b10778d8
Simon Blurton <sblurton@hotmail.co.uk>
2026-03-19 15:25:47 +0000

    Add recommended and accessibility rules for @angular-eslint/template

--- eslint.config.js
+++ eslint.config.js
@@ -68,6 +68,24 @@ export default [
       '@angular-eslint/template': angularEslintTemplatePlugin,
     },
     rules: {
+      // recommended
+      '@angular-eslint/template/banana-in-box': 'error',
+      '@angular-eslint/template/eqeqeq': 'error',
+      '@angular-eslint/template/no-negated-async': 'error',
+      '@angular-eslint/template/prefer-control-flow': 'error',
+      // accessibility
+      '@angular-eslint/template/alt-text': 'error',
+      '@angular-eslint/template/click-events-have-key-events': 'error',
+      '@angular-eslint/template/elements-content': 'error',
+      '@angular-eslint/template/interactive-supports-focus': 'error',
+      '@angular-eslint/template/label-has-associated-control': 'error',
+      '@angular-eslint/template/mouse-events-have-key-events': 'error',
+      '@angular-eslint/template/no-autofocus': 'error',
+      '@angular-eslint/template/no-distracting-elements': 'error',
+      '@angular-eslint/template/role-has-required-aria': 'error',
+      '@angular-eslint/template/table-scope': 'error',
+      '@angular-eslint/template/valid-aria': 'error',
+      // extra
       '@angular-eslint/template/use-track-by-function': 'warn',
     },
   },
